5c63c2736a3343cb9464b211ae5372c7ccf7d769
Jonathan Wong <jonathan.wong110@gmail.com>
2019-01-25 05:30:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,4 +14,10 @@ def move(board, index, player)
   board[index] = player
 end
 
-  
+def position_taken?(board, index)
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false 
+   else
+      return true
+   end
+end
