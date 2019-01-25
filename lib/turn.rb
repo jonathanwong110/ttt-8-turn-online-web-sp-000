2825a1e74f9d674bed1341837df3a293c117788c
Jonathan Wong <jonathan.wong110@gmail.com>
2019-01-25 05:56:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,5 +33,6 @@ def turn(board)
    user_input = gets.strip
    index = input_to_index(user_input)
    if valid_move?(board, index)
-     
+     move(board, index, current_player(board))
+    turn(board)
 end
