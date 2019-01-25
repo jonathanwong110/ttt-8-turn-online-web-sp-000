4215ca77f50dd6636e88bd784a710baa563fb2aa
Jonathan Wong <jonathan.wong110@gmail.com>
2019-01-25 05:32:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,3 +27,16 @@ def valid_move?(board, index)
       return true
    end
 end
+
+def turn_count(board)
+   counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+   counter
+end
+def current_player(board)
+   turn_count(board) % 2 == 0 ? "X" : "O"
+end
