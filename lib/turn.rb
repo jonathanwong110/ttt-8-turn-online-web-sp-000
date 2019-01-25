d551d58279fe36194beb6bd0e6975a5c35727dc1
Jonathan Wong <jonathan.wong110@gmail.com>
2019-01-25 05:29:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,56 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(integer)
-  integer.to_i - 1
-end
-
-def user_input(board, index, character)
-  index.between(0, 8)
-end
-
-def position_taken?(board, index)
-  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-    false
-  else
-    true
-  end
-end
-
-def valid_move?(board, index)
-  if index.between?(0, 8) && !(position_taken?(board, index))
-  true
-  end
-end
-
-def move(board, position, current_player)
-  update_array_at_with(board, position, current_player)
-end
-
-def update_array_at_with(array, index, value)
-  array[index] = value
-end
-
-def position_taken
-  if array[index] = "X" or "O"
-    puts "false"
-  end
-end
-
 def input_to_index(user_input)
   user_input.to_i - 1
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-end
-
-def turn(board)
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index, current_player)
-      move(board, index, current_player(board))
-      turn(board)
-   end
-   display_board(board)
 end
