41bf6914ead569e3b80ed7ec1dec8dc918b5495e
Jonathan Wong <jonathan.wong110@gmail.com>
2019-01-25 05:32:52 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,15 +28,13 @@ def valid_move?(board, index)
    end
 end
 
-def turn_count(board)
-   counter = 0
-   board.each do |spaces|
-      if spaces == "X" || spaces == "O"
-         counter += 1
-      end
+def turn(board)
+   puts "Please enter 1-9:"
+   user_input = gets.strip
+   index = input_to_index(user_input)
+   if valid_move?(board, index)
+      move(board, index, current_player(board))
+      turn(board)
    end
-   counter
-end
-def current_player(board)
-   turn_count(board) % 2 == 0 ? "X" : "O"
+   display_board(board)
 end
